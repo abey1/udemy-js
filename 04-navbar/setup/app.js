24a49c57276729504756ccadc957287a13db886a
abey1 <bruckabey@gmail.com>
2022-05-26 14:10:30 +0300

    challenge 4 navbar done

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,20 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+let menuOpened = false;
+
+const nav = document.getElementById("nav");
+const menu = document.getElementById("menu");
+
+window.addEventListener("resize", () => {
+  window.innerWidth >= 800 ? (nav.style.height = "50px") : null;
+});
+
+menu.addEventListener("click", () => {
+  window.innerWidth < 800
+    ? nav.style.height == "50px"
+      ? (nav.style.height = "200px")
+      : (nav.style.height = "50px")
+    : null;
+});
