14b5fbd4f5ca57d5f28f7e72bd3039c6830b8943
abey1 <bruckabey@gmail.com>
2022-05-27 13:19:19 +0300

    6th challenge finished

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,15 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const openModal = document.getElementById("btn");
+const modalContainer = document.getElementById("modal-container");
+const cross = document.getElementById("cross");
+
+openModal.addEventListener("click", () => {
+  modalContainer.style.display = "flex";
+});
+
+cross.addEventListener("click", () => {
+  modalContainer.style.display = "none";
+});
