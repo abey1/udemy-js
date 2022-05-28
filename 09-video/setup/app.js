9abb03bf3460cfcc388bf1a3407e90074cc3622c
abey1 <bruckabey@gmail.com>
2022-05-28 17:28:25 +0300

    additional corrections made on video challenge

--- 09-video/setup/app.js
+++ 09-video/setup/app.js
@@ -4,6 +4,7 @@ const playPauseOverlay = document.getElementById("play-pause-overlay");
 const myVideo = document.getElementById("myVideo");
 const controler = document.getElementById("controler");
 const textDisplayer = document.getElementById("text-displayer");
+const overlay = document.getElementById("overlay");
 
 playPauseButton.addEventListener("click", () => {
   if (playStatus) {
@@ -11,12 +12,14 @@ playPauseButton.addEventListener("click", () => {
     myVideo.pause();
     controler.style.display = "flex";
     textDisplayer.style.display = "flex";
+    overlay.style.display = "flex";
     playStatus = false;
   } else {
     playPauseOverlay.style.animation = "play 0.2s linear both";
     myVideo.play();
     controler.style.display = "none";
     textDisplayer.style.display = "none";
+    overlay.style.display = "none";
     playStatus = true;
   }
 });
